bf2b0800839d8edaa06b2e0c81d2c2976b376f9d
Subhradip Nandi <your-email@example.com>
2025-11-25 01:36:44 +0530

    Fix session cookies for cross-domain authentication

--- backend_auth/server.js
+++ backend_auth/server.js
@@ -32,6 +32,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
     }
   })
